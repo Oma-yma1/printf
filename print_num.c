ae920c7ce0d967c8d7516ce2b129fd1b9661bc5d
Oma-yma1 <zgani.omayma@gmail.com>
2023-04-24 14:48:08 -0700

    task1

--- print_num.c
+++ print_num.c
@@ -8,14 +8,27 @@ int print_num(va_list args)
 {
 int num = va_arg(args, int);
 int count = 0;
-char buffer[20];
+char *buffer;
 int i = 0;
+int temp = num;
 if (num < 0)
 {
 _putchar('-');
 count++;
 num = -num;
 }
+while (temp > 0)
+{
+temp /= 10;
+i++;
+}
+buffer = malloc(i + 1);
+if (!buffer)
+{
+fprintf(stderr, "Error: Out of memory\n");
+return (0);
+}
+i = 0;
 while (num > 0)
 {
 buffer[i++] = num % 10 + '0';
@@ -25,10 +38,11 @@ if (i == 0)
 {
 buffer[i++] = '0';
 }
-while (i > 0)
+while (i-- > 0)
 {
-_putchar(buffer[--i]);
+_putchar(buffer[i]);
 count++;
 }
+free(buffer);
 return (count);
 }
