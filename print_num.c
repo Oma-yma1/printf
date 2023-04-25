c62958cb54887aea42277143d46d7830e87ef8a8
tahaeddiani <tahaeddiani2@gmail.com>
2023-04-25 08:43:49 -0700

    task1

--- print_num.c
+++ print_num.c
@@ -6,43 +6,43 @@
  */
 int print_num(va_list args)
 {
-int num = va_arg(args, int);
+int n = va_arg(args, int);
 int count = 0;
-char *buffer;
-int i = 0;
-int temp = num;
-if (num < 0
-		)
+
+if (n < 0)
 {
-_putchar('-');
+	_putchar('-');
+	count++;
+	n = -n;
+}
+if (n == 0)
+{
+	_putchar('0');
+	return (++count);
+}
+if (n / 10)
+	count += print_num_helper(n / 10);
+
+_putchar(n % 10 + '0');
 count++;
-num = -num;
-}
-while (temp > 0)
-{
-temp /= 10;
-i++;
-}
-buffer = malloc(i + 1);
-if (!buffer)
-{
-return (-1);
-}
-i = 0;
-while (num > 0)
-{
-buffer[i++] = num % 10 + '0';
-num /= 10;
-}
-if (i == 0)
-{
-buffer[i++] = '0';
-}
-while (i-- > 0)
-{
-_putchar(buffer[i]);
-count++;
-}
-free(buffer);
+
 return (count);
+}
+
+/**
+ * print_num_helper - a helper function for print num
+ *
+ * @n: parameter n
+ * Return: return int
+ */
+int print_num_helper(int n)
+{
+	int count = 0;
+
+	if (n / 10)
+		count += print_num_helper(n / 10);
+	_putchar(n % 10 + '0');
+	count++;
+
+	return (count);
 }
